f42ed3ae132dabeadc9e0c7041af7d7a5c71749a
Robert Forkel <xrotwang@googlemail.com>
2017-11-21 12:54:25 +0100

    through the web! :)

---
+++ servers/fabfile.py
@@ -0,0 +1,13 @@
+import os
+
+from fabtools import require, service
+
+
+def munin_node():
+    require.deb.packages(['munin-node'])
+    require.users.user(
+        'munin-node', 
+        shell='/bin/bash', 
+        system=True, 
+        ssh_public_keys=[os.path.join(os.path.dirname(__file__) 'ssh_key_munin_node.pub')])
+    service.restart('munin-node')
