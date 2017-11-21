0517d2ca38e415de860830061d7be260462ead1f
chrzyki <christoph@functor-argument.net>
2017-11-21 15:12:08 +0100

    Added missing decorator.

--- servers/fabfile.py
+++ servers/fabfile.py
@@ -2,12 +2,16 @@ import os
 
 from fabtools import require, service
 
+from appconfig.tasks import *
 
+
+@task_app_from_environment
 def munin_node():
     require.deb.packages(['munin-node'])
     require.users.user(
         'munin-node', 
         shell='/bin/bash', 
         system=True, 
-        ssh_public_keys=[os.path.join(os.path.dirname(__file__), 'ssh_key_munin_node.pub')])
+        ssh_public_keys=[os.path.join(os.path.dirname(__file__),
+                                      'ssh_key_munin_node.pub')])
     service.restart('munin-node')
