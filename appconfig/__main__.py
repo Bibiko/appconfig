a9a859ea5f1c34c7ebeaebdcc46326f188729994
xrotwang <xrotwang@googlemail.com>
2017-11-24 13:48:49 +0100

    added command to test error reporting of apps and missing app dir for gelato

--- appconfig/__main__.py
+++ appconfig/__main__.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals, print_function
 
 import argparse
 
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.error import HTTPError
+
 from . import APPS
 
 
@@ -18,11 +21,24 @@ def ls():
         print(tmpl.format(*r))
 
 
+def test_error(appid):
+    """
+    Test the error reporting of an app by requesting its /_raise URL.
+    """
+    try:
+        urlopen('http://{0.domain}/_raise'.format(APPS[appid]))
+    except HTTPError as e:
+        assert e.code == 500
+
+
 def main():  # pragma: no cover
     parser = argparse.ArgumentParser(prog='appconfig', description='')
-    parser.add_argument('command', choices=['ls'])
+    parser.add_argument('command', choices=['ls', 'test_error'])
+    parser.add_argument('args', nargs=argparse.REMAINDER)
     args = parser.parse_args()
     if args.command == 'ls':
         ls()
+    elif args.command == 'test_error':
+        test_error(args.args[0])
     else:
         raise NotImplementedError
