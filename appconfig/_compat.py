689d79075c1313141db4fe496ac3580f5f6a32bf
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-12-05 14:40:39 +0100

    require pathlib2 on 3.4 so we can use read/write_text/bytes

--- appconfig/_compat.py
+++ appconfig/_compat.py
@@ -2,18 +2,21 @@
 
 import sys
 
+try:
+    import pathlib2 as pathlib
+except ImportError:
+    import pathlib
+
 PY2 = sys.version_info < (3,)
 
 
 if PY2:  # pragma: no cover
-    import pathlib2 as pathlib
     from urllib2 import urlopen, HTTPError
 
     iteritems = lambda x: x.iteritems()
     itervalues = lambda x: x.itervalues()
 
 else:  # pragma: no cover
-    import pathlib
     from urllib.request import urlopen, HTTPError
 
     iteritems = lambda x: iter(x.items())
