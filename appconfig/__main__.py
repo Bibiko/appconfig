aca39f33b06dfbe16bd1f1b9787020d6915a211f
xrotwang <xrotwang@googlemail.com>
2018-01-30 14:17:25 +0100

    add option to sort app list by port

--- appconfig/__main__.py
+++ appconfig/__main__.py
@@ -9,13 +9,14 @@ from ._compat import urlopen, HTTPError
 from . import APPS
 
 
-def ls():
+def ls(args):
     table = []
     for a in APPS.values():
         table.append((
             a.name,
             'http://{0}'.format(a.domain),
             a.production,
+            '{0}'.format(a.port),
             a.stack,
             '{0}'.format(a.public)))
         if a.test:
@@ -23,13 +24,20 @@ def ls():
                 '{0} [test]'.format(a.name),
                 'http://{0}/{1}'.format(a.test, a.name),
                 a.test,
+                '{0}'.format(a.port),
                 a.stack,
                 '{0}'.format(False)))
     cwidth = [2] + [max(map(len, c)) for c in zip(*table)]
     tmpl = ' '.join('{:%d}' % w for w in cwidth)
-    print(tmpl.format('#', 'id', 'url', 'server', 'stack', 'public'))
+    print(tmpl.format('#', 'id', 'url', 'server', 'port', 'stack', 'public'))
     print(tmpl.format(*('-' * w for w in cwidth)))
-    for i, r in enumerate(sorted(table, key=lambda t: (t[2], t[0]))):
+
+    if args and '-p' in args:
+        sortkey = lambda t: t[3]
+    else:
+        sortkey = lambda t: (t[2], t[0])
+
+    for i, r in enumerate(sorted(table, key=sortkey)):
         r = ['{0}'.format(i + 1)] + list(r)
         print(tmpl.format(*r))
 
@@ -53,7 +61,7 @@ def main():  # pragma: no cover
     args = parser.parse_args()
 
     if args.command == 'ls':
-        ls()
+        ls(args.args)
     elif args.command == 'test_error':
         test_error(args.args[0])
     else:
