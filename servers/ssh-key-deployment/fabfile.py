fc6485a30f2d4acf348b35b9ec02c5d8e43d1aa7
Christoph Rzymski <christoph@functor-argument.net>
2018-05-31 09:44:07 +0200

    Username check before host connection is more sensible.

--- servers/ssh-key-deployment/fabfile.py
+++ servers/ssh-key-deployment/fabfile.py
@@ -11,11 +11,11 @@ env.hosts = APPS.hostnames
 
 @task
 def add_key(user='', key=''):
+    if not user:
+        raise ValueError("Username cannot be empty.")
+
     if confirm("Add key/user to: " + env.host_string + "?"):
         key_path = os.path.join(os.path.dirname(__file__), key)
-
-        if not user:
-            raise ValueError("Username cannot be empty.")
 
         if not fabuser.exists(user):
             fabuser.create(user, password='changeme')
