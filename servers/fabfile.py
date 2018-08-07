63546d78a18ab0bf852085d5705d03c7eb895f5a
Christoph Rzymski <christoph@functor-argument.net>
2018-08-07 13:27:01 +0200

    Write requirements.txt files for all apps in APPS.ini, see #29
    Closes #29.

--- servers/fabfile.py
+++ servers/fabfile.py
@@ -1,4 +1,23 @@
-from fabric.api import task, sudo
+from fabric.api import run, task, sudo, settings
+from fabric.tasks import execute
+from fabtools import python
+
+from appconfig import APPS_DIR, APPS
+from appconfig.config import App
+
+
+def pip_freeze(app):
+    with python.virtualenv(str(app.venv_dir)):
+        stdout = run('pip freeze', combine_stderr=False)
+
+    def iterlines(lines):
+        for line in lines:
+            yield line + '\n'
+
+    target = APPS_DIR / app.name / 'requirements.txt'
+
+    with target.open('w', encoding='ascii') as fp:
+        fp.writelines(iterlines(stdout.splitlines()))
 
 
 @task
@@ -7,3 +26,18 @@ def ls():
     sudo('supervisorctl avail')
     sudo('psql -l', user='postgres')
 
+
+@task
+def pip_freeze_all():
+    """
+    Attempts to write requirements.txt files for all apps in apps.ini into
+    their respective apps folder.
+    """
+
+    def helper(app):
+        if type(app) is App:
+            execute(pip_freeze, app, host=app.production)
+
+    with settings(warn_only=True):
+        for a in APPS.values():
+            helper(a)
