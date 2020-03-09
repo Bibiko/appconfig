826e313da29f7bb47999d86fd73efcdbe8fc3e7b
chrzyki <christoph@functor-argument.net>
2020-03-09 17:05:19 +0100

    Add rudimentary task for buildbot deployment

--- apps/buildbot/fabfile.py
+++ apps/buildbot/fabfile.py
@@ -1,60 +1,106 @@
-import os
-import pathlib
+from fabric.api import env, sudo
+from fabtools import require, system, python
+from fabtools.require import git
 
-from fabric.api import local, env
-from fabric.contrib.console import confirm
-from fabtools import require, nginx, system, service
+from appconfig.tasks import task_app_from_environment, init
+from appconfig.tasks.deployment import require_venv
 
-from appconfig import systemd
-from appconfig.tasks import letsencrypt
-from appconfig.tasks import task_app_from_environment
-from appconfig.tasks.deployment import require_venv, require_nginx
+init()
 
 
 @task_app_from_environment
 def deploy(app):
-    assert system.distrib_id() == 'Ubuntu'
-    lsb_codename = system.distrib_codename()
-    if lsb_codename != 'xenial':
-        raise ValueError('unsupported platform: %s' % lsb_codename)
+    assert system.distrib_id() == "Ubuntu"
+    assert env.environment == "production"
 
-    # See whether the local appconfig clone is up-to-date with the remot master:
-    remote_repo = local('git ls-remote git@github.com:shh-dlce/appconfig.git HEAD | awk \'{ print $1}\'')
-    local_clone = local('git rev-parse HEAD')
-
-    if remote_repo != local_clone:
-        if confirm('Local appconfig clone is not up-to-date '
-                   'with remote master, continue?', default=False):
-            print("Continuing deployment.")
-        else:
-            print("Deployment aborted.")
-            return
-
-    require.deb.packages(getattr(app, 'require_deb_%s' % lsb_codename) + app.require_deb)
-    require.users.user(app.name, create_home=True, shell='/bin/bash')
-    require.directory(str(app.www_dir), use_sudo=True)
-    require.directory(str(app.www_dir / 'files'), use_sudo=True)
+    require.deb.packages(app.require_deb + ["libcairo2", "python3-venv"])
+    require.users.user(app.name, create_home=True, shell="/bin/bash")
 
     # Test and production instances are publicly accessible over HTTPS.
-    letsencrypt.require_certbot()
-    letsencrypt.require_cert(env.host)
-    if env.environment == 'production':
-        letsencrypt.require_cert(app)
+    #    letsencrypt.require_certbot()
+    #    letsencrypt.require_cert(env.host)
+    #    letsencrypt.require_cert(app)
 
-
-    #
-    # Create a virtualenv for the app and install the app package in development mode, i.e. with
-    # repository working copy in /usr/venvs/<APP>/src
-    #
-    require_venv(
-        app.venv_dir,
-        require_packages=[app.app_pkg] + app.require_pip,
+    git.working_copy(
+        "https://github.com/cldf/cldf-buildbot.git",
+        path=str(app.home_dir / "cldf-buildbot"),
+        user="buildbot",
+        use_sudo=True,
     )
 
-    require_nginx(dict(app=app))
+    git.working_copy(
+        "https://github.com/glottolog/glottolog.git",
+        path=str(app.home_dir / "glottolog"),
+        user="buildbot",
+        use_sudo=True,
+    )
+
+    git.working_copy(
+        "https://github.com/concepticon/concepticon-data.git",
+        path=str(app.home_dir / "concepticon-data"),
+        user="buildbot",
+        use_sudo=True,
+    )
+
+    git.working_copy(
+        "https://github.com/cldf-clts/clts.git",
+        path=str(app.home_dir / "clts"),
+        user="buildbot",
+        use_sudo=True,
+    )
+
+    # fabtools.git.checkout() for versions
+
+    require_venv(
+        app.venv_dir, requirements=str(app.home_dir / "cldf-buildbot" / "requirements.txt")
+    )
+
+    require.directory(str(app.home_dir / "master"), owner="buildbot", use_sudo=True)
+    require.directory(str(app.home_dir / "worker"), owner="buildbot", use_sudo=True)
+
+    require.files.file(
+        path=str(app.home_dir / "master" / "settings.py"),
+        owner="buildbot",
+        contents="HOST='141.5.108.108'",
+        use_sudo=True,
+        mode="777",
+    )
+
+    sudo(
+        "cp "
+        + str(app.home_dir / "cldf-buildbot" / "reposlist.json")
+        + " "
+        + str(app.home_dir / "master"),
+        user="buildbot",
+    )
+
+    sudo(
+        "cp "
+        + str(app.home_dir / "cldf-buildbot" / "config.py")
+        + " "
+        + str(app.home_dir / "master"),
+        user="buildbot",
+    )
+
+    with python.virtualenv(str(app.venv_dir)):
+        sudo("buildbot-worker stop " + str(app.home_dir / "worker"), user="buildbot")
+        sudo("buildbot create-master -c config.py " + str(app.home_dir / "master"), user="buildbot")
+        sudo("buildbot start " + str(app.home_dir / "master"), user="buildbot", warn_only=True)
+        sudo(
+            "buildbot-worker create-worker "
+            + str(app.home_dir / "worker")
+            + " "
+            + "localhost worker pass",
+            user="buildbot",
+        )
+        sudo(
+            "buildbot-worker start " + str(app.home_dir / "worker"), user="buildbot", warn_only=True
+        )
 
 
-    #start.execute_inner(app)
-    #check(app)
-    if env.environment == 'production':
-        systemd.enable(app, pathlib.Path(os.getcwd()) / 'systemd')
+#   require_nginx(dict(app=app))
+
+# start.execute_inner(app)
+# check(app)
+#   if env.environment == 'production':
+#       systemd.enable(app, pathlib.Path(os.getcwd()) / 'systemd')
