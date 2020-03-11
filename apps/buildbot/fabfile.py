fd726cb62eea90c903edf3aa8bc8524b671b1915
xrotwang <xrotwang@googlemail.com>
2020-03-11 16:41:40 +0100

    added fab task to change catalog version

--- apps/buildbot/fabfile.py
+++ apps/buildbot/fabfile.py
@@ -1,6 +1,8 @@
 import pathlib
+import functools
 
 from fabric.api import env, sudo, shell_env
+from fabric.context_managers import cd
 from fabtools import require, system, python
 from fabtools.require import git
 
@@ -9,7 +11,34 @@ from appconfig.tasks import letsencrypt
 from appconfig.tasks import task_app_from_environment, init
 from appconfig.tasks.deployment import require_venv, require_nginx
 
+CATALOGS = {
+    'glottolog': 'https://github.com/glottolog/glottolog',
+    'concepticon': 'https://github.com/concepticon/concepticon-data',
+    'clts': 'https://github.com/cldf-clts/clts',
+}
+
 init()
+
+
+def _ctl(cmd, warn_only=False):
+    for proc in ['master', 'worker']:
+        sudo("systemctl {0} buildbot-{1}".format(cmd, proc), warn_only=warn_only)
+
+
+start = functools.partial(_ctl, 'start')
+stop = functools.partial(_ctl, 'stop')
+
+
+@task_app_from_environment
+def catalogversions(app, **kw):
+    """
+    usage: fab catalogversions:production,glottolog=v4.1,concepticon=v2.3,clts=v1.4.1
+    """
+    for name, url in CATALOGS.items():
+        if name in kw:
+            with cd(str(app.home_dir / url.split('/')[-1])):
+                sudo('git fetch origin', user=app.name)
+                sudo('git checkout {}'.format(kw[name]), user=app.name)
 
 
 @task_app_from_environment
@@ -23,7 +52,6 @@ def deploy(app):
     with shell_env(SYSTEMD_PAGER=''):
         require.nginx.server()
 
-    # Test and production instances are publicly accessible over HTTPS.
     letsencrypt.require_certbot()
     letsencrypt.require_cert(env.host)
     letsencrypt.require_cert(app)
@@ -33,78 +61,44 @@ def deploy(app):
     git.working_copy(
         "https://github.com/cldf/cldf-buildbot.git",
         path=str(app.home_dir / "cldf-buildbot"),
-        user="buildbot",
+        user=app.name,
         use_sudo=True,
+        update=False,
     )
-
-    git.working_copy(
-        "https://github.com/glottolog/glottolog.git",
-        path=str(app.home_dir / "glottolog"),
-        user="buildbot",
-        use_sudo=True,
-    )
-
-    git.working_copy(
-        "https://github.com/concepticon/concepticon-data.git",
-        path=str(app.home_dir / "concepticon-data"),
-        user="buildbot",
-        use_sudo=True,
-    )
-
-    git.working_copy(
-        "https://github.com/cldf-clts/clts.git",
-        path=str(app.home_dir / "clts"),
-        user="buildbot",
-        use_sudo=True,
-    )
+    for name, url in CATALOGS.items():
+        git.working_copy(
+            url + ".git",
+            path=str(app.home_dir / url.split('/')[-1]),
+            user=app.name,
+            use_sudo=True,
+            update=False,
+        )
 
     # fabtools.git.checkout() for versions
 
     require_venv(
-        app.venv_dir, requirements=str(app.home_dir / "cldf-buildbot" / "requirements.txt")
-    )
+        app.venv_dir, requirements=str(app.home_dir / "cldf-buildbot" / "requirements.txt"))
 
-    require.directory(str(app.home_dir / "master"), owner="buildbot", use_sudo=True)
-    require.directory(str(app.home_dir / "worker"), owner="buildbot", use_sudo=True)
-
+    require.directory(str(app.home_dir / "master"), owner=app.name, use_sudo=True)
+    require.directory(str(app.home_dir / "worker"), owner=app.name, use_sudo=True)
     require.files.file(
         path=str(app.home_dir / "master" / "settings.py"),
-        owner="buildbot",
-        contents="HOST='buildbot.clld.org'",
+        owner=app.name,
+        contents="URL='https://{}'".format(app.domain),
         use_sudo=True,
         mode="777",
     )
 
-    sudo(
-        "cp "
-        + str(app.home_dir / "cldf-buildbot" / "reposlist.json")
-        + " "
-        + str(app.home_dir / "master"),
-        user="buildbot",
-    )
+    bbdo = functools.partial(sudo, user=app.name)
+    bbdo("cp {0.home_dir}/cldf-buildbot/reposlist.json {0.home_dir}/master".format(app))
+    bbdo("cp {0.home_dir}/cldf-buildbot/config.py {0.home_dir}/master".format(app))
 
-    sudo(
-        "cp "
-        + str(app.home_dir / "cldf-buildbot" / "config.py")
-        + " "
-        + str(app.home_dir / "master"),
-        user="buildbot",
-    )
-
-    sudo("systemctl stop buildbot-master", warn_only=True)
-    sudo("systemctl stop buildbot-worker", warn_only=True)
+    stop(warn_only=True)
 
     with python.virtualenv(str(app.venv_dir)):
-        sudo("buildbot create-master -c config.py " + str(app.home_dir / "master"), user="buildbot")
-        sudo(
-            "buildbot-worker create-worker "
-            + str(app.home_dir / "worker")
-            + " "
-            + "localhost worker pass",
-            user="buildbot",
-        )
+        bbdo("buildbot create-master -c config.py {0.home_dir}/master".format(app))
+        bbdo("buildbot-worker create-worker {0.home_dir}/worker localhost worker pass".format(app))
 
     systemd.enable(app, pathlib.Path(__file__).parent / "systemd")
     sudo("systemctl daemon-reload", warn_only=True)
-    sudo("systemctl start buildbot-master", warn_only=True)
-    sudo("systemctl start buildbot-worker", warn_only=True)
+    start()
